c6746620bd55e5f8ee53031f59bdd82da6fdbb00
Jisan129 <jishananam@iut-dhaka.edu>
2020-12-09 18:47:49 +0600

    Completed the migration of the table in the database

---
+++ app/quizTable.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class quizTable extends Model
+{
+
+    protected $table="quiz_table";
+    protected $primaryKey="quiz_id";
+    public $timestamps=false;
+
+    //
+}
