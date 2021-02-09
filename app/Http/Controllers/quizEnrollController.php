078b7f1c41c49530d83394bd2baee6e4cabe12d9
Jisan129 <jishananam@iut-dhaka.edu>
2021-02-09 18:52:23 +0600

    At last Teacher Can Update

--- app/Http/Controllers/quizEnrollController.php
+++ app/Http/Controllers/quizEnrollController.php
@@ -32,7 +32,7 @@ class quizEnrollController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -44,7 +44,7 @@ class quizEnrollController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -55,7 +55,7 @@ class quizEnrollController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
@@ -66,63 +66,97 @@ class quizEnrollController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request)
     {
         //
-        $id=$request->input('question_id');
-        $getQuestion=questionTable::where('question_id',$id)->get();
-        return view('Teacher.updateInput',compact('id'))->with('getQuestion',$getQuestion);
+        $id = $request->input('question_id');
+        $getQuestion = questionTable::where('question_id', $id)->get();
+        return view('Teacher.updateInput', compact('id'))->with('getQuestion', $getQuestion);
     }
 
-    public function updateQuestion(Request $request){
-        $exam_id=$request->input('exam_code');
+    public function updateQuestion(Request $request)
+    {
+        $exam_id = $request->input('exam_code');
         print_r($exam_id);
-        $answerScript=questionTable::where('exam_id',$exam_id)->get();
-        return view('Teacher.updateQuestion',compact('exam_id'))->with('answerScript',$answerScript);
+        $answerScript = questionTable::where('exam_id', $exam_id)->get();
+        return view('Teacher.updateQuestion', compact('exam_id'))->with('answerScript', $answerScript);
     }
+
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
         //
     }
-    public function showChart(Request $request){
-        $quiz_id=$request->input('quiz_id');
-        $result=quizEnroll::where('total_marks',$quiz_id)->pluck('student_name','result');
+
+    public function showChart(Request $request)
+    {
+        $quiz_id = $request->input('quiz_id');
+        $result = quizEnroll::where('total_marks', $quiz_id)->pluck('student_name', 'result');
 
         //print_r($result);
-        $chart=new resultChart;
+        $chart = new resultChart;
         $chart->labels($result->values());
-        $chart->dataset('By the Name of Allah','bar',$result->keys());
-        return view('chart.charts',compact('chart'));
+        $chart->dataset('By the Name of Allah', 'bar', $result->keys());
+        return view('chart.charts', compact('chart'));
 
     }
-    public function interMediateReq(Request $request){
-        $temp=$request->input('teacher_id');
-        return view('Teacher.addExam',compact('temp'));
 
+    public function interMediateReq(Request $request)
+    {
+        $temp = $request->input('teacher_id');
+        return view('Teacher.addExam', compact('temp'));
 
 
     }
-    public function showUpdate(Request $request){
-        $idNumber=$request->input('teacher_id');
-        return view('Teacher.updateIntermediate',compact('idNumber'));
+
+    public function showUpdate(Request $request)
+    {
+        $idNumber = $request->input('teacher_id');
+        return view('Teacher.updateIntermediate', compact('idNumber'));
     }
 
 
-    public function updateLast(Request $request){
-        $title=$request->input('question_name');
+    public function updateLast(Request $request)
+    {
+        $title = $request->input('question_name');
         print_r(10);
     }
-    public function ok(){
-        print_r('nO');
+
+    public function ok(Request $request)
+    {
+        $temp = $request->input('exam_id');
+        print_r($temp);
+        $table = questionTable::where('question_id', $temp)
+            ->update(['correct_answer' => $request->input('correct_answer')]);
+
+        $table = questionTable::where('question_id', $temp)
+            ->update(['option_A' => $request->input('option_A')]);
+        $table = questionTable::where('question_id', $temp)
+            ->update(['option_B' => $request->input('option_B')]);
+        $table = questionTable::where('question_id', $temp)
+            ->update(['option_C' => $request->input('option_C')]);
+        $table = questionTable::where('question_id', $temp)
+            ->update(['option_D' => $request->input('option_D')]);
+        $table = questionTable::where('question_id', $temp)
+            ->update(['question_title' => $request->input('question_title')]);
+
+
+
+
+
+        print_r($request->input('option_A'));
+        print_r($request->input('option_B'));
+        print_r($request->input('option_C'));
+        print_r($request->input('option_D'));
+
     }
 }
--- resources/views/Teacher/updateInput.blade.php
+++ resources/views/Teacher/updateInput.blade.php
@@ -1,27 +1,20 @@
 @extends('layouts.app')
 @section('content')
-        <form action="final_update" method="post">
-            @csrf
-        @foreach($getQuestion as $question)
-            <h1>{{$question->question_title}}</h1>
-            <input type="text" id="question_name" name="question_name">
-            <label for="question_name">Question</label><br>
-            <input type="text" id="option_A" name="option_A">
-            <label for="option_A">Option A</label><br>
-            <input type="text" id="option_B" name="option_B">
-            <label for="option_B">Option B</label><br>
-            <input type="text" id="option_C" name="option_C">
-            <label for="option_C">Option C</label><br>
-            <input type="text" id="option_D" name="option_D">
-            <label for="option_D">Option D</label><br>
-            <input type="text" id="correctAnswer" name="correctAnswer">
-            <label for="correctAnswer">Correct Answer</label><br>
-            <br>
-            <input type="submit" name="final_update" id="final_update">
+
+    @foreach($getQuestion as $answer)
+    <form action="please" method="post">
+        @csrf
+        <h1>{{$answer->question_id}}</h1>
+        <input type="hidden" name="exam_id" value="{{$answer->question_id}}">
+        <input type="text" name="question_title">
+        <input type="text" name="option_A">
+        <input type="text" name="option_B">
+        <input type="text" name="option_C">
+        <input type="text" name="option_D">
+        <input type="text" name="correct_answer">
+        <input type="submit" name="please"  >
 
 
     </form>
     @endforeach
-
-
 @endsection
